0f89052adeb65867276c7b65d84180477d00303c
knarfytrebil <knarfytrebil@gmail.com>
2023-11-29 20:26:29 +1030

    (fix) remove excessive library

--- index.jsx
+++ index.jsx
@@ -2,6 +2,6 @@ import store from './state/store.js'
 import { render } from 'preact-render-to-string';
 import { App } from './App.jsx'
 
-const str = render(<App what="is this"/>);
+const str = render(<App what="is this" />);
 
-console.log(str);
+console.log(str)
